5d0b0c85a94ff813452130ab91ac9ad8672247e0
kbetter3 <kbetter3@naver.com>
2018-03-26 16:15:43 +0900

    Member Class ver.1

---
+++ MidtermProj/src/sw4/team2/client/Member.java
@@ -0,0 +1,61 @@
+package sw4.team2.client;
+
+import java.io.Serializable;
+
+public class Member implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String pw;
+	private int type;
+	
+	public Member(String id, String pw, int type) {
+		this.id = id;
+		this.pw = pw;
+		this.type = type;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+}
