3e8f48ad290ee3af5a3d28e4446efff34c49121a
kbetter3 <kbetter3@naver.com>
2018-03-29 21:47:36 +0900

    add empty factory method

--- MidtermProj/src/sw4/team2/client/ItemButton.java
+++ MidtermProj/src/sw4/team2/client/ItemButton.java
@@ -5,129 +5,73 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class ItemButton extends JButton {
-	private boolean isBig = false;
-	private ImageIcon[] normalImage, pressedImage;
+	private ImageIcon bgIcon;
+	private BufferedImage normalImg, pressedImg, bgImg;
+	private MouseListener listener;
 	
-	public ItemButton(ImageIcon[] normalImage, ImageIcon[] pressedImage) {
-		super();
-	
-		this.normalImage = normalImage;
-		this.pressedImage = pressedImage;
+	public ItemButton(ImageIcon icon, int width, int height) {
+		super(icon);
 		
+		this.bgIcon = icon;
+		
+		try {
+			this.bgImg = this.normalImg = ImageIO.read(new File("img/normalBtn.png"));
+			this.pressedImg = ImageIO.read(new File("img/pressedBtn.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		this.setSize(width, height);
 		this.setOpaque(false);
 		this.setContentAreaFilled(false);
 		this.setBorder(null);
 		
-		this.setIcon(normalImage[0]);
+		this.setIcon(ImageResizer.fit(this, bgIcon));
 		
 		event();
-	}
-	
-	public boolean getIsBig() {
-		return isBig;
-	}
-	
-	public ImageIcon[] getNormalImage() {
-		return normalImage;
-	}
+		this.repaint();
+	}	
 
-	public ImageIcon[] getPressedImage() {
-		return pressedImage;
-	}
-	
 	private void event() {
-		this.addMouseListener(new MouseAdapter(){
+		listener = new MouseAdapter(){
 			@Override
 			public void mousePressed(MouseEvent e) {
-				/*
-				if (isBig)
-					ItemButton.this.setIcon(pressedImage[4]);
-				else
-					ItemButton.this.setIcon(pressedImage[0]);
-				*/
-				
-				ItemButton.this.setIcon(fit(ItemButton.this, pressedImage[0]));
+				bgImg = pressedImg;
+				ItemButton.this.repaint();
 			}
+			
 			@Override
 			public void mouseReleased(MouseEvent e) {
-				/*
-				if (isBig)
-					ItemButton.this.setIcon(normalImage[4]);
-				else
-					ItemButton.this.setIcon(normalImage[0]);
-				*/
-				
-				ItemButton.this.setIcon(fit(ItemButton.this, normalImage[0]));
+				bgImg = normalImg;
+				ItemButton.this.repaint();
 			}
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				BtnThread bt;
-				bt = new BtnThread(ItemButton.this, ItemButton.this.getNormalImage(), ItemButton.this.getIsBig());
-				bt.setDaemon(true);
-				bt.start();
-				
-				isBig = !isBig;
-			}
-		});
+		};
+		this.addMouseListener(listener);
 	}
 	
-	class BtnThread extends Thread {
-		private ItemButton btn;
-		private ImageIcon[] normalIcon;
-		private boolean isBig;
-		
-		public BtnThread(ItemButton btn, ImageIcon[] normalIcon, boolean isBig) {
-			this.btn = btn;
-			this.normalIcon = normalIcon;
-			this.isBig = isBig;
-		}
-		
-		@Override
-		public void run() {
-			for (int i = 1; i < 5; i++) {
-				if (isBig)
-					small(i);
-				else
-					big(i);
-				
-				try {
-					Thread.sleep(80);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		private void small(int i) {
-			btn.setSize(btn.getWidth() - 4, btn.getHeight() - 4);
-//			btn.setIcon(normalIcon[4 - i]);
-			btn.setPreferredSize(new Dimension(btn.getPreferredSize().width - 4, btn.getPreferredSize().height - 4));
-			btn.setIcon(fit(btn, normalIcon[0]));
-		}
-		
-		private void big(int i) {
-			btn.setSize(btn.getWidth() + 4, btn.getHeight() + 4);
-//			btn.setIcon(normalIcon[i]);
-			btn.setPreferredSize(new Dimension(btn.getPreferredSize().width + 4, btn.getPreferredSize().height + 4));
-			btn.setIcon(fit(btn, normalIcon[0]));
-		}
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.drawImage(bgImg, 0, 0, ItemButton.this.getWidth(), ItemButton.this.getHeight(), ItemButton.this);
+		g.drawImage(ImageResizer.fitImage(ItemButton.this, bgIcon), 0, 0, ItemButton.this);
 	}
 	
-	private ImageIcon fit(Component c, ImageIcon icon) {
-		int width = c.getWidth();
-		int height = c.getHeight();
+	public static ItemButton emptyBtn() {
+//		ItemButton btn = new ItemButton(new ImageIcon("img/emptyBtn.png"), 74, 74);
+		ItemButton btn = new ItemButton(new ImageIcon("img/GREEN CREAMED MINT.png"), 74, 74);
 		
-		BufferedImage resize = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		Graphics g = resize.getGraphics();
-		g.drawImage(icon.getImage(), 0, 0, width, height, null);
-		
-		return new ImageIcon(resize);
+		btn.setEnabled(false);
+		btn.removeMouseListener(btn.listener);
+		return btn;
 	}
-	
 }
