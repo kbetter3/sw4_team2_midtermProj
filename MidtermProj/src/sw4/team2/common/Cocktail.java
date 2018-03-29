25419bafe7654cd5d39210759f464a3bda1ff815
kbetter3 <kbetter3@naver.com>
2018-03-29 18:00:30 +0900

    add serializable

--- MidtermProj/src/sw4/team2/common/Cocktail.java
+++ MidtermProj/src/sw4/team2/common/Cocktail.java
@@ -1,8 +1,9 @@
 package sw4.team2.common;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class Cocktail {
+public class Cocktail implements Serializable {
 	String name;
 	List<Recipe> recipe;
 }
