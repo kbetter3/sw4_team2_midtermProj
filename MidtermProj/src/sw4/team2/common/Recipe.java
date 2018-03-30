c4a9eef9435b815a9cb4e9c845c73418e0d2cbf2
kbetter3 <kbetter3@naver.com>
2018-03-30 15:50:13 +0900

    include Item class

--- MidtermProj/src/sw4/team2/common/Recipe.java
+++ MidtermProj/src/sw4/team2/common/Recipe.java
@@ -5,23 +5,25 @@ import java.io.Serializable;
 public class Recipe implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private String name;
-	private String engName;
-	private int beverageType;
+	private Item item;
 	private boolean beverageGroup;
-	private int type;
 	private String answer;
 	
-	public static final int TYPE_GLASS = 0;
-	public static final int TYPE_BEVERAGE = 1;
-	public static final int TYPE_GARNISHES = 2;
-	public static final int TYPE_ADDITIONAL = 3;
-	public static final int TYPE_BARSOPPN = 4;
-	public static final int TYPE_COCKTAILPICK = 5;
-	public static final int TYPE_SHAKER = 6;
-	public static final int TYPE_MIXINGGLASS = 7;
-	public static final int TYPE_STRAINER = 8;
-	public static final int TYPE_BLENDER = 9;
-	
-	public static final int BEVERAGE_GIN = 0;
+	public Recipe(Item item, boolean beverageGroup, String answer) {
+		this.item = item;
+		this.beverageGroup = beverageGroup;
+		this.answer = answer;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public boolean isBeverageGroup() {
+		return beverageGroup;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
 }
